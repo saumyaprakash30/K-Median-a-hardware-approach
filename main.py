949181ee24acca707ffa209cfc956290029006ae
saumyaprakash30 <saumyaprakash30@gmail.com>
2019-11-07 23:49:23 +0530

    added adder Function

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ from matplotlib import style
 style.use('ggplot')
 import numpy as np
 
+def adder(a,b):
+	upperCode = "`include \"fulladder16bit.v\"\nmodule top;\nreg [15:0] a,b;\nreg ci;\nwire [15:0] sum;\nwire co;\nfulladder16bit f(a,b,ci,sum,co);\ninitial\nbegin\n"
+	variable = "a=16'd"+str(a)+";b=16'd"+str(b)+";ci=1'b0;\n";
+	lowerCode  ="end\ninitial\nbegin\n\n$monitor(\"%d\",sum);\nend\nendmodule\n"
+
+	testBench = upperCode+variable+lowerCode
+	f=open("adder.v","w")
+	f.write(testBench)
+	f.close()
+	cmd = "iverilog "+"adder.v "
+	os.system(cmd)
+	cmd ="./a.out > adderResult.txt"
+	os.system(cmd)
+	f =open("adderResult.txt","r")
+	res = f.read()
+	res=res.strip()
+	return int(res)
+
+
+
 X = np.array([[1, 2],
               [1.5, 1.8],
               [5, 8 ],
