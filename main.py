af0a44f609823a9b96ad891a120616ee5e511107
saumyaprakash30 <saumyaprakash30@gmail.com>
2019-11-12 11:32:02 +0530

    removed comment

--- main.py
+++ main.py
@@ -78,9 +78,7 @@ def adder(a,b):
 
 #///
 def subtractor(a,b):
-	# upperCode = "`include \"fulladder16bit.v\"\nmodule top;\nreg [15:0] a,b;\nreg ci;\nwire [15:0] sum;\nwire co;\nfulladder16bit f(a,b,ci,sum,co);\ninitial\nbegin\n"
-	# variable = "a=16'd"+str(a)+";b=16'd"+str(b)+";ci=1'b0;\n";
-	# lowerCode  ="end\ninitial\nbegin\n\n$monitor(\"%d\",sum);\nend\nendmodule\n"
+
 
 	upperCode = "`include \"fullsubtractor64bit.v\"\nmodule top;\nreg [63:0] a,b;\nreg ci;\nwire [63:0] sum;\nwire co;\nfullsubtractor64bit f(a,b,ci,sum,co);\ninitial\nbegin\n"
 	variable = "a=64'd"+str(a)+";b=64'd"+str(b)+";ci=1'b1;\n";
